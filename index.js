14cd8c63bea6b0cad144f910fce19ab099f50c13
MaxChang3 <maxcfoo@gmail.com>
2022-10-07 11:24:30 +0800

    refactor: use injector to insert css (#4)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const fs = require('hexo-fs');
 const path = require('path');
-
+const css = hexo.extend.helper.get('css').bind(hexo);
 const bili = require('./lib/bili.js')
 
 hexo.extend.generator.register('bili_asset', ()=>[
@@ -18,8 +18,6 @@ hexo.extend.tag.register("bilicard",  async(args)=> {
     return await bili(args[0]);
 },{async: true});
 
-hexo.extend.filter.register('after_post_render', (data) => {
-    let my_css = '<link rel="stylesheet" href="/css/bilicard.css" type="text/css">';
-    data.content += my_css;
-    return data;
-});
+hexo.extend.injector.register('head_begin', ()=> {
+  return css('/css/bilicard.css');
+}, 'post')
