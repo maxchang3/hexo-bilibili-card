3966313eb7a9e7402c147c4d452151b3cdd23af5
MaxChang3 <maxcfoo@gmail.com>
2022-10-07 11:34:49 +0800

    style: format code

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require('path');
 const css = hexo.extend.helper.get('css').bind(hexo);
 const bili = require('./lib/bili.js')
 
-hexo.extend.generator.register('bili_asset', ()=>[{
-      path: 'css/bilicard.css',
-      data: () => fs.createReadStream(path.resolve(__dirname, "./source" ,"bilicard.css"))
-    }
+hexo.extend.generator.register('bili_asset', () => [{
+  path: 'css/bilicard.css',
+  data: () => fs.createReadStream(path.resolve(__dirname, "./source", "bilicard.css"))
+}
 ]);
 
-hexo.extend.tag.register("bilicard",  async(args)=> {
-    return await bili(args[0]);
-},{async: true});
+hexo.extend.tag.register("bilicard", async (args) => {
+  return await bili(args[0]);
+}, { async: true });
 
-hexo.extend.injector.register('head_begin', ()=> {
+hexo.extend.injector.register('head_begin', () => {
   return css('/css/bilicard.css');
 }, 'post')
